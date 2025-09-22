0642b2335b01bfececf6f8b5643a8cc5aa1b9434
SofiJess <sofijess@SofiJesss-Mac-mini.local>
2025-09-22 12:12:00 +0100

    fix: camera and player should move together now

--- Build/20250914-1232.loader.js
+++ Build/20250914-1232.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 95965953,
+    wasmFileSize: 95966020,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
